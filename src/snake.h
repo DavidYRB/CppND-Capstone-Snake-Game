404f8e613b64cc456d6bb6b0e38074148e9210be
ruibo <david.yanrb@gmail.com>
2020-05-07 11:48:49 -0400

    * Modify Snake constructor to be able take customized initial speed and size * Used std::queue to represent snake body instead of vector * Moved member variables as private which can only be accessed from member functions

--- src/snake.h
+++ src/snake.h
@@ -1,36 +1,36 @@
 #ifndef SNAKE_H
 #define SNAKE_H
 
-#include <vector>
+#include <queue>
 #include "SDL.h"
 
 class Snake {
  public:
   enum class Direction { kUp, kDown, kLeft, kRight };
 
-  Snake(int grid_width, int grid_height)
+  Snake(int grid_width, int grid_height, float speed=0.1f, int initial_size = 1)
       : grid_width(grid_width),
         grid_height(grid_height),
         head_x(grid_width / 2),
-        head_y(grid_height / 2) {}
+        head_y(grid_height / 2), 
+        speed(speed),
+        size(initial_size) {}
 
-  void Update();
-
+  void UpdatePose();
   void GrowBody();
   bool SnakeCell(int x, int y);
-
-  Direction direction = Direction::kUp;
-
-  float speed{0.1f};
-  int size{1};
-  bool alive{true};
-  float head_x;
-  float head_y;
-  std::vector<SDL_Point> body;
 
  private:
   void UpdateHead();
   void UpdateBody(SDL_Point &current_cell, SDL_Point &prev_cell);
+
+  Direction direction = Direction::kUp;
+  float speed;
+  int size;
+  bool alive{true};
+  float head_x;
+  float head_y;
+  std::queue<SDL_Point> body;
 
   bool growing{false};
   int grid_width;
